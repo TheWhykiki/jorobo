07c28e34eae64d4519438884453c918486547cff
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2021-09-02 13:56:44 +0200

    Fix missing symlink to current on creating zip file

--- src/Tasks/Build/Module.php
+++ src/Tasks/Build/Module.php
@@ -63,6 +63,9 @@ class Module extends Base implements TaskInterface
 		// Prepare directories
 		$this->prepareDirectories();
 
+		// Create symlink to current folder
+		$this->createSymlink($this->target, JPATH_BASE . "/dist/current");
+
 		$files = $this->copyTarget($this->source, $this->target);
 
 		// Build media (relative path)
